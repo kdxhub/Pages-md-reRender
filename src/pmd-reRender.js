f490ab84ddbf776fac8c3db300d30d51d8973ca7
kdxiaoyi <kdxiaoyi@outlook.com>
2025-03-12 15:31:42 +0000

    修改配置格式为更加灵活的JSON

--- src/pmd-reRender.js
+++ src/pmd-reRender.js
@@ -1,46 +1,102 @@
 // 将Github Pages自动生成的Markdown渲染页面进行自动重绘
 // Powered by SoberJS
-// 自定义设置项区 详见文档：https://kdxiaoyi.top/Pages-md-reRender/global-conf
-const /*文章授权协议*/conf_licen=`CC BY-NC 4.0`;
-const /*文章授权协议链接*/conf_licen_link=`https://creativecommons.org/licenses/by-nc/4.0/legalcode.zh-hans`;
-const /*在代码块下方添加复制代码按钮*/conf_codeCopyBtn=true;
-const   /*代码块复制按钮默认文本*/conf_codeCopyBtn_tip="Copy";
-const   /*代码块复制按钮点击后文本*/conf_codeCopyBtn_tip_done="Copied!";
-const /*允许点击图片来查看大图*/conf_imgView=true;
-const   /*启用查看大图对imgse图床的优化*/conf_imgView_imgse=true;
-const   /*启用查看大图查看原图 跳转至imgse查看页而不是源文件*/conf_imgView_imgse_noRes=true;
-const   /*启用查看大图查看原图按钮*/conf_imgView_open=true;
-const /*启用建站时长计时 [是否启用t/f,年,月,日]*/conf_time=[false,2022,7,20];
-const /*左侧边栏·一言*/conf_saying=`Keep the spirit of Touching 𝕏.`;
-const /*左侧边栏·第1格·背景图片*/conf_sidebar_headimg_src=``;
-const   /*左侧边栏·第1格·背景图片描述*/conf_sidebar_headimg_alt=``;
-const /*左侧边栏·第2格内容*/conf_sidebar_links=`
-<s-chip id="side_ship_0" onclick="openURL('/','')" clickable="true" class="sidebar_btn">
-<s-icon slot="start" type="home"></s-icon>
-主页 Homepage</s-chip>
-<s-chip id="side_ship_1" onclick="openURL('/blogs','')" clickable="true" class="sidebar_btn">
-<s-icon slot="start"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M320-320h480v-120H698q-21 37-58 58.5T560-360q-42 0-79-21.5T422-440H320v120Zm240-120q34 0 57-23.5t23-56.5h160v-280H320v280h160q0 33 23.5 56.5T560-440ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-240h480-480Z"></path></svg></s-icon>
-博客 Blog</s-chip>
-<s-chip id="side_ship_2" onclick="openURL('/project','')" clickable="true" class="sidebar_btn">
-<s-icon slot="start"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-183v-274L200-596v274l240 139Zm80 0 240-139v-274L520-457v274Zm-40-343 237-137-237-137-237 137 237 137ZM160-252q-19-11-29.5-29T120-321v-318q0-22 10.5-40t29.5-29l280-161q19-11 40-11t40 11l280 161q19 11 29.5 29t10.5 40v318q0 22-10.5 40T800-252L520-91q-19 11-40 11t-40-11L160-252Zm320-228Z"></path></svg></s-icon>
-项目 Project</s-chip>
-<s-chip id="side_ship_3" onclick="openURL('https://space.bilibili.com/1')" clickable="true" class="sidebar_btn">
-<s-icon slot="start"><svg width="100px" height="100px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"/><path d="M18.223 3.086a1.25 1.25 0 0 1 0 1.768L17.08 5.996h1.17A3.75 3.75 0 0 1 22 9.747v7.5a3.75 3.75 0 0 1-3.75 3.75H5.75A3.75 3.75 0 0 1 2 17.247v-7.5a3.75 3.75 0 0 1 3.75-3.75h1.166L5.775 4.855a1.25 1.25 0 1 1 1.767-1.768l2.652 2.652c.079.079.145.165.198.257h3.213c.053-.092.12-.18.199-.258l2.651-2.652a1.25 1.25 0 0 1 1.768 0zm.027 5.42H5.75a1.25 1.25 0 0 0-1.247 1.157l-.003.094v7.5c0 .659.51 1.199 1.157 1.246l.093.004h12.5a1.25 1.25 0 0 0 1.247-1.157l.003-.093v-7.5c0-.69-.56-1.25-1.25-1.25zm-10 2.5c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25zm7.5 0c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25z"/></g></svg></s-icon>
-Bilibili ↗</s-chip>`;
-const /*左侧边栏·第2格内容中没有按文档编写请启用此项*/conf_sidebar_links_preventDefault=false;
-const /*复制文本后向文本末尾添加一段文本，详见文档*/conf_copy_endnote=` ‖ 来自[%ETITLE%](%LINK%)，以${conf_licen}协议授权。`;
-const   /*阻止一些简易解除复制限制脚本工作并提示用户*/conf_prenevtOnclickOverwrite=true;
-const /*图片加载失败后的占位符图片*/conf_img_error_replace="https://rs.kdxiaoyi.top/res/images/load_err.svg";
-const /*为所有在新标签页打开的链接添加右上箭头*/conf_link_arrow=true;
-const   /*仅对含有 ↗ 或 $ 的链接生效*/conf_link_arrow_replace=true;
-const   /*如果链接含有 ฿ 则将其修改为新标签页打开*/conf_link_target_replace=true;
-const   /*外链图标*/conf_link_arrow_icon=`<s-icon class="newWindowOpen"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"></path></svg></s-icon>`;
-const /*自定义边栏内容，禁用保持留空*/conf_replaceSidebar=``;
-const /*启用目录统计，高级用法详见文档*/conf_index=true;
-const /*启用目录索引侧栏*/conf_index_sidebar=true;
-const /*在标题的最后添加一个按钮以复制链接指向这个标题*/conf_headerLinkCopyBtn=true;
-const /*在页面底端增加文章脚注，为空不额外添加*/conf_footer=``;
-const /*检查引用部分高级语法，详见文档*/conf_quotepro=[true,`#1A73E7`,`#FBC116`,`#E23B2E`,`#30C496`];
+// 自定义设置项区 详见文档：https://kdxiaoyi.top/Pages-md-reRender/global-conf (有待更新)
+const conf = {
+  info: {
+    /*启用建站时长计时 [是否启用t/f,年,月,日]*/
+    time: [false,2022,7,20],
+    /*左侧边栏·一言*/
+    saying: `Keep the spirit of Touching 𝕏.`,
+    licen: {
+      /*文章授权协议*/
+      what: `CC BY-NC 4.0`,
+      /*文章授权协议链接*/
+      link: `https://creativecommons.org/licenses/by-nc/4.0/legalcode.zh-hans`,
+    },
+  },
+  code: {
+  /*在代码块下方添加复制代码按钮*/
+    enabled: true,
+    /*代码块复制按钮默认文本*/
+    tip: "Copy",
+    /*代码块复制按钮点击后文本*/
+    done: "Copied!",
+  },
+  img: {
+    view: {
+      /*允许点击图片来查看大图*/
+      enabled: true,
+      /*启用查看大图查看原图按钮*/
+      open: true,
+    },
+    imgse_com: {
+      /*启用查看大图对imgse图床的优化*/
+      enabled: true,
+      /*启用查看大图查看原图 跳转至imgse查看页而不是源文件*/
+      detail: true,
+    },
+    /*图片加载失败后的占位符图片*/
+    error: "https://rs.kdxiaoyi.top/res/images/load_err.svg",
+  },
+  sidebar: {
+    solt_1: {
+      /*左侧边栏·第1格·背景图片*/
+      src: ``,
+      /*左侧边栏·第1格·背景图片描述*/
+      alt: ``,
+    },
+    solt_2: {
+      /*左侧边栏·第2格内容*/
+      innerHTML:`
+        <s-chip id: "side_ship_0" onclick: "openURL('/','')" clickable: "true" class: "sidebar_btn">
+        <s-icon slot: "start" type: "home"></s-icon>
+        主页 Homepage</s-chip>
+        <s-chip id: "side_ship_1" onclick: "openURL('/blogs','')" clickable: "true" class: "sidebar_btn">
+        <s-icon slot: "start"><svg xmlns: "http://www.w3.org/2000/svg" viewBox: "0 -960 960 960"><path d: "M320-320h480v-120H698q-21 37-58 58.5T560-360q-42 0-79-21.5T422-440H320v120Zm240-120q34 0 57-23.5t23-56.5h160v-280H320v280h160q0 33 23.5 56.5T560-440ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-240h480-480Z"></path></svg></s-icon>
+        博客 Blog</s-chip>
+        <s-chip id: "side_ship_2" onclick: "openURL('/project','')" clickable: "true" class: "sidebar_btn">
+        <s-icon slot: "start"><svg xmlns: "http://www.w3.org/2000/svg" viewBox: "0 -960 960 960"><path d: "M440-183v-274L200-596v274l240 139Zm80 0 240-139v-274L520-457v274Zm-40-343 237-137-237-137-237 137 237 137ZM160-252q-19-11-29.5-29T120-321v-318q0-22 10.5-40t29.5-29l280-161q19-11 40-11t40 11l280 161q19 11 29.5 29t10.5 40v318q0 22-10.5 40T800-252L520-91q-19 11-40 11t-40-11L160-252Zm320-228Z"></path></svg></s-icon>
+        项目 Project</s-chip>
+        <s-chip id: "side_ship_3" onclick: "openURL('https://space.bilibili.com/1')" clickable: "true" class: "sidebar_btn">
+        <s-icon slot: "start"><svg width: "100px" height: "100px" viewBox: "0 0 24 24" xmlns: "http://www.w3.org/2000/svg"><g><path fill: "none" d: "M0 0h24v24H0z"/><path d: "M18.223 3.086a1.25 1.25 0 0 1 0 1.768L17.08 5.996h1.17A3.75 3.75 0 0 1 22 9.747v7.5a3.75 3.75 0 0 1-3.75 3.75H5.75A3.75 3.75 0 0 1 2 17.247v-7.5a3.75 3.75 0 0 1 3.75-3.75h1.166L5.775 4.855a1.25 1.25 0 1 1 1.767-1.768l2.652 2.652c.079.079.145.165.198.257h3.213c.053-.092.12-.18.199-.258l2.651-2.652a1.25 1.25 0 0 1 1.768 0zm.027 5.42H5.75a1.25 1.25 0 0 0-1.247 1.157l-.003.094v7.5c0 .659.51 1.199 1.157 1.246l.093.004h12.5a1.25 1.25 0 0 0 1.247-1.157l.003-.093v-7.5c0-.69-.56-1.25-1.25-1.25zm-10 2.5c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25zm7.5 0c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25z"/></g></svg></s-icon>
+        Bilibili ↗</s-chip>`,
+      /*左侧边栏·第2格内容中没有按文档编写请启用此项*/
+      sidebar_links_preventDefault: false,
+    },
+    /*自定义边栏内容，禁用保持留空*/
+    replacement: ``,
+  },
+  copy: {
+  },
+  hyper_markdown: {
+    /*在标题的最后添加一个按钮以复制链接指向这个标题*/
+    header_link: true,
+    /*在页面底端增加文章脚注，为空不额外添加*/
+    footer: ``,
+    /*检查引用部分高级语法，详见文档*/
+    quotepro: [true,`#1A73E7`,`#FBC116`,`#E23B2E`,`#30C496`],
+  },
+  link: {
+    arrow: {
+      /*为所有在新标签页打开的链接添加右上箭头*/
+      enabled: true,
+      /*仅对含有 ↗ 或 $ 的链接生效*/
+      replace: true,
+      /*如果链接含有 ฿ 则将其修改为新标签页打开*/
+      target_replace: true,
+      /*外链图标*/
+      icon: `<s-icon class: "newWindowOpen"><svg xmlns: "http://www.w3.org/2000/svg" viewBox: "0 -960 960 960"><path d: "M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"></path></svg></s-icon>`,
+    },
+  },
+  index: {
+    /*启用目录统计，高级用法详见文档*/
+    enabled: true,
+    /*启用目录索引侧栏*/
+    sidebar: true,
+  },
+};
+/*在复制的文本结尾追加文字，见文档*/
+conf.copy.endnote = ` ‖ 来自[%ETITLE%](%LINK%)，以${conf.info.licen.what}协议授权。`;
 
 const /*插件版本（建议不要修改）*/PluginVer=["2.0.0",18];
 
@@ -83,10 +139,10 @@ document.body.innerHTML = `
     <s-drawer>
       <div slot="start" style="width: auto;">
             <s-card type="" class="sidebar_head">
-              <div slot="image"><img data-ui-img="true" src="${conf_sidebar_headimg_src}"></div>
-              <div slot="headline"><span class='sidebar_username_bg'>${conf_sidebar_headimg_alt}</span></div>
+              <div slot="image"><img data-ui-img="true" src="${conf.sidebar.solt_1.src}"></div>
+              <div slot="headline"><span class='sidebar_username_bg'>${conf.sidebar.solt_1.alt}</span></div>
             </s-card><br>
-            <s-card type="" class="sidebar_head">${conf_sidebar_links}</s-card><br>
+            <s-card type="" class="sidebar_head">${conf.sidebar.solt_2.innerHTML}</s-card><br>
             <s-card type="" class="sidebar_head" id="index_links_sidebarSlot" style="display:none;">
               <s-fold folded="true">
                 <s-chip slot="trigger" clickable="true" class="sidebar_btn">
@@ -97,14 +153,14 @@ document.body.innerHTML = `
               </s-fold>
             </s-card><br>
             <s-card type="" class="sidebar_head">
-              <div id="saying" class="selectable"><center>${conf_saying}</center></div>
+              <div id="saying" class="selectable"><center>${conf.info.saying}</center></div>
               <div id="time"><center><small>Since 2022-07-19</small></center></div>
-              <div id="license"><center><small>以<a href="${conf_licen_link}">${conf_licen}</a>协议提供内容</small></center></div>
+              <div id="license"><center><small>以<a href="${conf.info.licen.link}">${conf.info.licen.what}</a>协议提供内容</small></center></div>
             </s-card>
       </div>
       <s-appbar>
         <s-tooltip slot="navigation">
-          <s-icon-button onclick="document.querySelector('s-drawer').toggle()">
+          <s-icon-button type="filled-tonal" onclick="document.querySelector('s-drawer').toggle()">
             <s-icon name="menu"></s-icon>
           </s-icon-button>
           切换侧栏
@@ -114,8 +170,8 @@ document.body.innerHTML = `
           Title Detailed
         </s-tooltip>
         <s-tooltip slot="action">
-          <s-icon-button id="toTopBtn" slot="trigger">
-            <s-icon name="dark_mode"></s-icon>
+          <s-icon-button type="outlined" id="toTopBtn" slot="trigger">
+            <s-icon name="chevron_up"></s-icon>
           </s-icon-button>
           回到顶部
         </s-tooltip>
