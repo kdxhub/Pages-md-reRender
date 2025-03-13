7fd1e1a1681971e6c19c6d0d8ab0b6b14007fd8f
kdxiaoyi <kdxiaoyi@outlook.com>
2025-03-13 16:25:23 +0000

    调整代码格式

--- src/pmd-reRender.js
+++ src/pmd-reRender.js
@@ -56,17 +56,17 @@ const conf = {
     solt_2: {
       /*左侧边栏·第2格内容*/
       innerHTML:`
-        <s-chip id: "side_ship_0" onclick: "openURL('/','')" clickable: "true" class: "sidebar_btn">
-        <s-icon slot: "start" type: "home"></s-icon>
+        <s-chip id="side_ship_0" onclick="openURL('/','')" clickable="true" class="sidebar_btn">
+        <s-icon slot="start" type="home"></s-icon>
         主页 Homepage</s-chip>
-        <s-chip id: "side_ship_1" onclick: "openURL('/blogs','')" clickable: "true" class: "sidebar_btn">
-        <s-icon slot: "start"><svg xmlns: "http://www.w3.org/2000/svg" viewBox: "0 -960 960 960"><path d: "M320-320h480v-120H698q-21 37-58 58.5T560-360q-42 0-79-21.5T422-440H320v120Zm240-120q34 0 57-23.5t23-56.5h160v-280H320v280h160q0 33 23.5 56.5T560-440ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-240h480-480Z"></path></svg></s-icon>
+        <s-chip id="side_ship_1" onclick="openURL('/blogs','')" clickable="true" class="sidebar_btn">
+        <s-icon slot="start"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M320-320h480v-120H698q-21 37-58 58.5T560-360q-42 0-79-21.5T422-440H320v120Zm240-120q34 0 57-23.5t23-56.5h160v-280H320v280h160q0 33 23.5 56.5T560-440ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-240h480-480Z"></path></svg></s-icon>
         博客 Blog</s-chip>
-        <s-chip id: "side_ship_2" onclick: "openURL('/project','')" clickable: "true" class: "sidebar_btn">
-        <s-icon slot: "start"><svg xmlns: "http://www.w3.org/2000/svg" viewBox: "0 -960 960 960"><path d: "M440-183v-274L200-596v274l240 139Zm80 0 240-139v-274L520-457v274Zm-40-343 237-137-237-137-237 137 237 137ZM160-252q-19-11-29.5-29T120-321v-318q0-22 10.5-40t29.5-29l280-161q19-11 40-11t40 11l280 161q19 11 29.5 29t10.5 40v318q0 22-10.5 40T800-252L520-91q-19 11-40 11t-40-11L160-252Zm320-228Z"></path></svg></s-icon>
+        <s-chip id="side_ship_2" onclick="openURL('/project','')" clickable="true" class="sidebar_btn">
+        <s-icon slot="start"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-183v-274L200-596v274l240 139Zm80 0 240-139v-274L520-457v274Zm-40-343 237-137-237-137-237 137 237 137ZM160-252q-19-11-29.5-29T120-321v-318q0-22 10.5-40t29.5-29l280-161q19-11 40-11t40 11l280 161q19 11 29.5 29t10.5 40v318q0 22-10.5 40T800-252L520-91q-19 11-40 11t-40-11L160-252Zm320-228Z"></path></svg></s-icon>
         项目 Project</s-chip>
-        <s-chip id: "side_ship_3" onclick: "openURL('https://space.bilibili.com/1')" clickable: "true" class: "sidebar_btn">
-        <s-icon slot: "start"><svg width: "100px" height: "100px" viewBox: "0 0 24 24" xmlns: "http://www.w3.org/2000/svg"><g><path fill: "none" d: "M0 0h24v24H0z"/><path d: "M18.223 3.086a1.25 1.25 0 0 1 0 1.768L17.08 5.996h1.17A3.75 3.75 0 0 1 22 9.747v7.5a3.75 3.75 0 0 1-3.75 3.75H5.75A3.75 3.75 0 0 1 2 17.247v-7.5a3.75 3.75 0 0 1 3.75-3.75h1.166L5.775 4.855a1.25 1.25 0 1 1 1.767-1.768l2.652 2.652c.079.079.145.165.198.257h3.213c.053-.092.12-.18.199-.258l2.651-2.652a1.25 1.25 0 0 1 1.768 0zm.027 5.42H5.75a1.25 1.25 0 0 0-1.247 1.157l-.003.094v7.5c0 .659.51 1.199 1.157 1.246l.093.004h12.5a1.25 1.25 0 0 0 1.247-1.157l.003-.093v-7.5c0-.69-.56-1.25-1.25-1.25zm-10 2.5c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25zm7.5 0c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25z"/></g></svg></s-icon>
+        <s-chip id="side_ship_3" onclick="openURL('https://space.bilibili.com/1')" clickable="true" class="sidebar_btn">
+        <s-icon slot="start"><svg width="100px" height="100px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"/><path d="M18.223 3.086a1.25 1.25 0 0 1 0 1.768L17.08 5.996h1.17A3.75 3.75 0 0 1 22 9.747v7.5a3.75 3.75 0 0 1-3.75 3.75H5.75A3.75 3.75 0 0 1 2 17.247v-7.5a3.75 3.75 0 0 1 3.75-3.75h1.166L5.775 4.855a1.25 1.25 0 1 1 1.767-1.768l2.652 2.652c.079.079.145.165.198.257h3.213c.053-.092.12-.18.199-.258l2.651-2.652a1.25 1.25 0 0 1 1.768 0zm.027 5.42H5.75a1.25 1.25 0 0 0-1.247 1.157l-.003.094v7.5c0 .659.51 1.199 1.157 1.246l.093.004h12.5a1.25 1.25 0 0 0 1.247-1.157l.003-.093v-7.5c0-.69-.56-1.25-1.25-1.25zm-10 2.5c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25zm7.5 0c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25z"/></g></svg></s-icon>
         Bilibili ↗</s-chip>`,
       /*左侧边栏·第2格内容中没有按文档编写请启用此项*/
       preventDefault: false,
@@ -242,7 +242,7 @@ document.body.innerHTML = `
     </s-tooltip>
   </s-appbar>
   <s-drawer id="_pmd-mainContent" class="custom-scroll">
-    <div id="_pmd-LeftSiderbar" slot="start" style="width: auto;">
+    <div id="_pmd-LeftSiderbar" slot="start">
       <s-card id="_pmd-slot_1" type="" class="sidebar_head">
         <div slot="image"><img data-ui-img="true" src="${conf.sidebar.solt_1.src}"></div>
         <div slot="headline"><span class='sidebar_username_bg'>${conf.sidebar.solt_1.alt}</span></div>
@@ -271,6 +271,7 @@ document.body.innerHTML = `
 <!-- 页面重渲染插入代码结束 -->
 `;
 
+//pmd元素常量组
 const pmdElements = {
   pageRoot: document.getElementById("_pmd-pageRoot"),
   appbar: {
@@ -316,26 +317,31 @@ const pmdElements = {
   pageConfig: document.getElementById("mdRender_config"),
 };
 
+//通用函数
+function openURL(URL, IsInPresentWindow) { if (IsInPresentWindow != undefined) { link_a.target = "_self"; } else { link_a.target = "_blank"; }; link_a.href = URL; link_a.click(); };
+function getQueryString(name) { let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); let r = window.location.search.substr(1).match(reg); if (r != null) { return unescape(r[2]); }; return null; };
+function msg(Message, ConfirmText, isWarning) { let infoJson = {}; infoJson.root = document.querySelector('s-page'); infoJson.text = Message; if (ConfirmText == undefined) { infoJson.action = ""; } else { infoJson.action = ConfirmText; }; if (isWarning != undefined) { infoJson.type = "error"; }; customElements.get('s-snackbar').show(infoJson);};
+
 //title动画和回顶按钮显隐
-pmdElements.appbar.toTopBtn.addEventListener("animationend", (event) => {if (pmdElements.appbar.toTopBtn.className == "fadeOut") {pmdElements.appbar.toTopBtn.style="display: none;";};});
+pmdElements.appbar.toTopBtn.addEventListener("animationend", (event) => { if (pmdElements.appbar.toTopBtn.className == "fadeOut") { pmdElements.appbar.toTopBtn.style = "display: none;"; }; });
 function refreshAppbar() {
   /*修改UITitsle的透明度*/
   if (document.documentElement.scrollTop / pmdElements.content.origin.header.root.scrollHeight <= 1.5) {
-    pmdElements.appbar.title.style.opacity=document.documentElement.scrollTop / pmdElements.content.origin.header.root.scrollHeight;
+    pmdElements.appbar.title.style.opacity = document.documentElement.scrollTop / pmdElements.content.origin.header.root.scrollHeight;
   };
   /*滚过一屏后显示回顶按钮的动画*/
   if (document.documentElement.scrollTop >= window.innerHeight) {
     if (pmdElements.appbar.toTopBtn.className != "fadeIn") {
-      pmdElements.appbar.toTopBtn.setAttribute("onclick","scrollToTop();");
-      pmdElements.appbar.toTopBtn.setAttribute("class","fadeIn");
-      pmdElements.appbar.toTopBtn.style="";
+      pmdElements.appbar.toTopBtn.setAttribute("onclick", "scrollToTop();");
+      pmdElements.appbar.toTopBtn.setAttribute("class", "fadeIn");
+      pmdElements.appbar.toTopBtn.style = "";
     };
   } else {
     if (pmdElements.appbar.toTopBtn.className != "fadeOut") {
-      pmdElements.appbar.toTopBtn.setAttribute("onclick","javascript:void(0);");
-      pmdElements.appbar.toTopBtn.setAttribute("class","fadeOut");
-      pmdElements.appbar.toTopBtn.style="display: none;";
-      pmdElements.appbar.toTopBtn.style="";
+      pmdElements.appbar.toTopBtn.setAttribute("onclick", "javascript:void(0);");
+      pmdElements.appbar.toTopBtn.setAttribute("class", "fadeOut");
+      pmdElements.appbar.toTopBtn.style = "display: none;";
+      pmdElements.appbar.toTopBtn.style = "";
     };
   };
 };
@@ -343,12 +349,12 @@ window.addEventListener("scroll", refreshAppbar);
 
 //检查页面设置元素并应用
 if (!!pmdElements.pageConfig) {
-  if ((!conf.sidebar.solt_2.preventDefault)&&(Math.floor(pmdElements.pageConfig.dataset.sideshipHide) >= 0)) {
+  if ((!conf.sidebar.solt_2.preventDefault) && (Math.floor(pmdElements.pageConfig.dataset.sideshipHide) >= 0)) {
     /* sideship-hide Int 禁用指定边栏链接 */
-    let sideShipBtn=document.getElementById("side_ship_"+Math.floor(pmdElements.pageConfig.dataset.sideshipHide))
+    let sideShipBtn = document.getElementById("side_ship_" + Math.floor(pmdElements.pageConfig.dataset.sideshipHide))
     sideShipBtn.setAttribute("type", "filled-tonal");
     sideShipBtn.setAttribute("clickable", "false");
-    sideShipBtn.setAttribute("onclick", "void(0);");s
+    sideShipBtn.setAttribute("onclick", "void(0);"); s
   };
   if (pmdElements.pageConfig.hasAttribute("data-title")) {
     /* title Str 强制覆写UI标题，若不存在则使用文章标题 */
@@ -374,4 +380,4 @@ window.addEventListener('scroll', updateCustomScroll);
 //页面初始化
 refreshAppbar();
 updateCustomScroll();
-console.log('%cPages Markdown Re-Render v'+PluginVer[0]+'%c['+PluginVer[1]+'%c]\nCopyright (C) 2024 kdxiaoyi. All right reserved.','color:#90BBB1;','color:#90BBB1;','color:#90BBB1;');
+console.log('%cPages Markdown Re-Render v' + PluginVer[0] + '%c[' + PluginVer[1] + '%c]\nCopyright (C) 2024 kdxiaoyi. All right reserved.', 'color:#90BBB1;', 'color:#90BBB1;', 'color:#90BBB1;');
