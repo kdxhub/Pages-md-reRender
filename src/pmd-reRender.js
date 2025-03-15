3c412255d79455f03fc2fc68f4229bd8507d8529
kdxiaoyi <kdxiaoyi@outlook.com>
2025-03-15 14:09:01 +0000

    添加View on Github按钮转移功能

--- src/pmd-reRender.js
+++ src/pmd-reRender.js
@@ -23,6 +23,8 @@ const conf = {
     },
     /*KDX-Shared User Interface标识符（启用后会在同样使用了pmd的网站间共享用户侧设置）*/
     kdxSharedUI: true,
+    /*允许将正文内的View on Github按钮转移*/
+    view_on_github: true,
   },
   code: {
   /*在代码块下方添加复制代码按钮*/
@@ -182,6 +184,10 @@ document.body.innerHTML = `
   .main-content {
     max-width: 100%;
   }
+  .page-header {
+    background-color: rgba(255, 255, 255, 0);
+    background-image: linear-gradient(120deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0));
+  }
 </style><style id="_pmd-style-darkmode">
   @media not (prefers-color-scheme: dark) {
     body {
@@ -243,18 +249,18 @@ document.body.innerHTML = `
     </s-tooltip>
     <div id="_pmd-pageTitle" slot="headline"> Title </div>
     <s-tooltip slot="action">
-      <s-icon-button id="_pmd-githubBtn" style="display: none;" type="outlined" slot="trigger">
-        <s-icon></s-icon>
-      </s-icon-button>
-      在 Github.com 上查看
-    </s-tooltip>
-    <s-tooltip slot="action">
       <s-icon-button id="_pmd-toTopBtn" style="display: none;" type="outlined" slot="trigger">
         <s-icon name="chevron_up"></s-icon>
       </s-icon-button>
       回到顶部
     </s-tooltip>
-  </s-appbar>
+    <s-tooltip slot="action">
+      <s-icon-button id="_pmd-githubBtn" style="display: none;" type="none" slot="trigger">
+        <s-icon><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path></svg></s-icon>
+      </s-icon-button>
+      在 Github.com 上查看
+    </s-tooltip>
+    </s-appbar>
   <s-drawer id="_pmd-mainContent" class="custom-scroll">
     <div id="_pmd-LeftSiderbar" slot="start">
       <s-card id="_pmd-slot_1" type="" class="sidebar_head">
@@ -303,6 +309,9 @@ const pmdElements = {
     custom: document.getElementById("_pmd-style-custom"),
   },
   appbar: {
+    _: {
+      GithubLink: "",
+    },
     root: document.getElementById("_pmd-appbarRoot"),
     menuBtn: document.getElementById("_pmd-menuBtn"),
     title: document.getElementById("_pmd-pageTitle"),
@@ -347,6 +356,7 @@ const pmdElements = {
         root: document.getElementsByClassName("page-header")[0],
         main: document.getElementsByClassName("project-name")[0],
         sub: document.getElementsByClassName("project-tagline")[0],
+        view_on_github: document.querySelector("#_pmd-originalContent > header > a.btn"),
       },
       main: {
         root: document.getElementById("content"),
@@ -392,6 +402,14 @@ function msg(Message, ConfirmBtnText, isWarning, duration, onclick, align, icon)
   customElements.get("s-snackbar").builder(infoJSON);
   return infoJSON;
 };
+
+//移动View on Github按钮
+if (conf.info.view_on_github && !!pmdElements.content.origin.header.view_on_github) {
+  pmdElements.appbar._.GithubLink = pmdElements.content.origin.header.view_on_github.href;
+  pmdElements.appbar.Github.addEventListener("click", () => {openURL(pmdElements.appbar._.GithubLink, false);});
+  pmdElements.appbar.Github.style = "";
+};
+if (!!pmdElements.content.origin.header.view_on_github) {pmdElements.content.origin.header.view_on_github.remove();};
 
 //title动画和回顶按钮显隐
 pmdElements.appbar.toTopBtn.addEventListener("animationend", (event) => { if (pmdElements.appbar.toTopBtn.className == "fadeOut") { pmdElements.appbar.toTopBtn.style = "display: none;"; }; });
